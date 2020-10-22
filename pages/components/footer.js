72edd4a17becd98898c77adce566e93ed660257a
trunghieu_210376 <trunghieu_210376@yahoo.com>
2020-10-22 20:43:06 +0700

    22/10/2020

---
+++ pages/components/footer.js
@@ -0,0 +1,17 @@
+const Footer=() => {
+    return(
+        <div>
+
+<footer className="py-5 bg-dark">
+                <div className="container">
+                    <p className="m-0 text-center text-white">
+                        Copyright &copy; Your Website 2019
+                    </p>
+                </div>
+            </footer>
+        </div>
+       
+    )
+}
+
+export default Footer
