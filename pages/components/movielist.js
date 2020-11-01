060398f8ff10df2b711244c039a92639ad2c6f53
Nguyen Hieu <trunghieu_219376@yahoo.com>
2020-11-01 18:05:49 +0700

    01/11/2020_2

--- pages/components/movielist.js
+++ pages/components/movielist.js
@@ -1,35 +1,47 @@
 import React from "react";
 
-class MoveiList extends React.Component {
+class MoveList extends React.Component {
+    shorten = (text, maxLength) => {
+        if (text && text.length > maxLength) {
+            return text.substr(0, maxLength) + "...";
+        }
+        return text.substr(0, 50) + "...";
+    };
     renderMovies() {
-        const { movies } = this.props;
-        return movies.map((movie) => (
-            <div className="col-lg-4 col-md-6 mb-4">
-                <div className="card h-100">
-                    <a href="#">
-                        <img
-                            className="card-img-top"
-                            src={movie.image}
-                            alt=""
-                        />
-                    </a>
-                    <div className="card-body">
-                        <h4 className="card-title">
-                            <a href="#">{movie.name}</a>
-                        </h4>
-                        <h5>$24.99</h5>
-                        <p className="card-text">{movie.description}</p>
-                    </div>
-                    <div className="card-footer">
-                        <small className="text-muted">{movie.rating}</small>
+        if (this.props.movies) {
+            const { movies } = this.props;
+            var commentNodes = movies.map((movie) => (
+                
+                <div className="col-lg-4 col-md-6 mb-4" key={movie.id}>
+                    <div className="card h-100">
+                        <a href="#">
+                            <img
+                                className="card-img-top"
+                                src={movie.image}
+                                alt=""
+                            />
+                        </a>
+                        <div className="card-body">
+                            <h4 className="card-title">
+                                <a href="#">{movie.name}</a>
+                            </h4>
+                            <h5>$24.99</h5>
+                            <p className="card-text">
+                                {this.shorten(movie.description, 100)}
+                            </p>
+                        </div>
+                        <div className="card-footer">
+                            <small className="text-muted">{movie.rating}</small>
+                        </div>
                     </div>
                 </div>
-            </div>
-        ));
+            ));
+        }
+        return commentNodes;
     }
     render() {
         return <React.Fragment>{this.renderMovies()}</React.Fragment>;
     }
 }
 
-export default MoveiList;
+export default MoveList;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from "react";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
 import Narbar from "./components/narbar";
@@ -5,87 +6,105 @@ import SideMenu from "./components/sideMenu";
 import Carousel from "./components/carousel";
 import MoveList from "./components/movielist";
 import Footer from "./components/footer";
+import { getMovies } from "../actions";
 
+class Home extends React.Component {
+    // const [movies, setMovies] = useState([]);
+    // const [count, setCount] = useState(0);
+    // useEffect(() => {
+    //     const fetchData = async () => {
+    //         const resMovies = await getMovies();
+    //         setMovies(resMovies);
+    //     };
+    //     fetchData();
+    // }, [count]);
+    constructor(props) {
+        super(props);
+        this.state = {
+            movies: [],
+            errorMessage: ""
+        };
+    }
 
-export default function Home() {
+    // async componentDidMount() {
+    //     const movies = await getMovies();
+    //     this.setState({ movies });
+    // }
 
-    const MOVIE_DATA = [
-        {
-          id: '1',
-          name: 'The Shawshank Redemption',
-          releaseYear: 1994,
-          description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
-          rating: 4.8,
-          genre: 'drama',
-          image: 'https://m.media-amazon.com/images/M/MV5BNjQ2NDA3MDcxMF5BMl5BanBnXkFtZTgwMjE5NTU0NzE@._V1_CR0,60,640,360_AL_UX477_CR0,0,477,268_AL_.jpg'
-        },
-        {
-          id: '2',
-          name: 'The Dark Knight',
-          releaseYear: 2008,
-          description: 'When the menace known as The Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham. The Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
-          rating: 4.7,
-          genre: 'action, crime, drama',
-          image: 'https://img.cinemablend.com/filter:scale/quill/c/3/8/0/f/4/c380f4f12cfeec19f0c40c6f57db188f2f98cca8.jpg?mw=600'
-        },
-        {
-          id: '3',
-          name: 'Lord of the Rings',
-          releaseYear: 2004,
-          description: 'A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.',
-          rating: 4.9,
-          genre: 'adventure, drama, fantasy',
-          image: 'https://img.cinemablend.com/filter:scale/quill/0/f/5/2/a/6/0f52a6843a25c1a5c1f9a0c00548cad9e1d912e2.jpg?mw=600'
-        }
-      ]
-   
-    return (
-        <div>
-            <Head>
-                <title>Home</title>
-                <link
-                    rel="stylesheet"
-                    href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
-                    integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
-                    crossOrigin="anonymous"
-                />
-                <script
-                    src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
-                    integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
-                    crossOrigin="anonymous"
-                ></script>
-                <script
-                    src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
-                    integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
-                    crossOrigin="anonymous"
-                ></script>
-                <script
-                    src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
-                    integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
-                    crossOrigin="anonymous"
-                ></script>
-            </Head>
+    async componentDidMount() {
+        getMovies()
+            .then((movies) => {
+                this.setState({ movies });
+            })
+            .catch((error) => {
+                this.setState({ errorMessage: error });
+            });
+    }
 
-            <Narbar />
+    render() {
+        const { movies, errorMessage } = this.state;
+        return (
+            <div>
+                <Head>
+                    <title>Home</title>
+                    <link
+                        rel="stylesheet"
+                        href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+                        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+                        crossOrigin="anonymous"
+                    />
+                    <script
+                        src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+                        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+                        crossOrigin="anonymous"
+                    ></script>
+                    <script
+                        src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+                        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+                        crossOrigin="anonymous"
+                    ></script>
+                    <script
+                        src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+                        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+                        crossOrigin="anonymous"
+                    ></script>
+                </Head>
 
-            <div className="container">
-                <div className="row">
-                    <div className="col-lg-3">
-                        <SideMenu />
-                    </div>
+                <Narbar />
 
-                    <div className="col-lg-9">
-                        <Carousel />
-                        <div className="row">
-                            <MoveList  movies={MOVIE_DATA}/>
+                <div className="container">
+                    <button
+                        onClick={() => {
+                            setCount(count + 1);
+                        }}
+                    >
+                        Click me !
+                    </button>
 
+                    <div className="row">
+                        <div className="col-lg-3">
+                            <SideMenu />
                         </div>
-                       
-                       
+
+                        <div className="col-lg-9">
+                            <Carousel />
+                            <div className="row">
+                                {errorMessage && (
+                                    <div
+                                        className="alert alert-danger"
+                                        role="alert"
+                                    >
+                                        {errorMessage}
+                                    </div>
+                                )}
+                                <MoveList movies={movies} />
+                            </div>
+                        </div>
                     </div>
                 </div>
+                <Footer />
             </div>
-            <Footer/>
-        </div>
-    );
+        );
+    }
 }
+export default Home;
